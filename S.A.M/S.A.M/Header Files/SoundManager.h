c403d6b16fd883daee91978a4ec94793d327d79d
Oscar Andersson <oscar@niceguyandersson.com>
2016-01-27 10:14:38 +0100

    Working soundstuff

--- S.A.M/S.A.M/Header Files/SoundManager.h
+++ S.A.M/S.A.M/Header Files/SoundManager.h
@@ -2,6 +2,19 @@
 #define _SOUNDMANAGER_H_
 #include "fmod.hpp"
 #include "fmod_errors.h"
+#include <vector>
+
+//Cool as fuck flags for the loadfunction
+enum SoundFlags
+{
+	LOAD_MEMORY = 0x01,
+	LOAD_STREAM = 0x02
+};
+
+inline SoundFlags operator|(SoundFlags a, SoundFlags b)
+{
+	return static_cast<SoundFlags>(static_cast<int>(a) | static_cast<int>(b));
+}
 
 class SoundManager {
 	//Functions
@@ -10,7 +23,9 @@ class SoundManager {
 		void FMODErrorCheck(FMOD_RESULT result);			//For checking the results of FMOD functions
 		void MessageBoxAndShutDown(std::stringstream* _ss); //Called if something went wrong so the program needs to shut down
 	public:
-
+		SoundManager();
+		void LoadSound(char* fileName, char* soundName, SoundFlags flags);	//Loads sound into array 
+		void PlayOneShotSound(char* soundName);								//Plays a sound with a specific name
 	//Variables
 	private:
 		FMOD::System* m_system;				//Core of FMOD
@@ -21,6 +36,8 @@ class SoundManager {
 		FMOD_CAPS m_caps;					//??? Soundcard capabilities ???
 		char m_name[256];					//Name of sound driver
 
+		std::vector<FMOD::Sound*> m_sounds;	//All the sounds
+		std::vector<char*> m_soundIndex;	//Soundnames, where soundname index is the corresponding index of m_sounds
 };
 
 #endif // !_SOUNDMANAGER_H_
--- S.A.M/S.A.M/Source Files/SoundManager.cpp
+++ S.A.M/S.A.M/Source Files/SoundManager.cpp
@@ -5,6 +5,10 @@
 
 //using namespace FMOD;
 
+SoundManager::SoundManager() {
+	InitFMOD();
+}
+
 void SoundManager::MessageBoxAndShutDown(std::stringstream* _ss) {
 	std::string _boxStringS(_ss->str());
 	const char* _boxStringLPCSTR = _boxStringS.c_str();
@@ -16,6 +20,52 @@ void SoundManager::MessageBoxAndShutDown(std::stringstream* _ss) {
 		MB_ICONERROR | MB_OK);
 	exit(-1);
 }
+void SoundManager::LoadSound(char* fileName, char* soundName, SoundFlags flags)
+{
+	FMOD::Sound* audio;
+
+	//Either load into memory or as a stream
+	if (flags == LOAD_MEMORY)
+		m_result = m_system->createSound(fileName, FMOD_DEFAULT, 0, &audio);
+	else if (flags == LOAD_STREAM)
+		m_result = m_system->createStream(fileName, FMOD_DEFAULT, 0, &audio);
+	else	//No mode set
+	{
+		MessageBox(NULL, (LPCSTR)L"No Loadmode Set", "Sound Loading Error", MB_ICONERROR | MB_OK);
+		exit(-1);
+	}
+	
+	FMODErrorCheck(m_result);
+
+	//Add audio and sound name to their vectors
+	m_sounds.push_back(audio);
+	m_soundIndex.push_back(soundName);
+}
+
+void SoundManager::PlayOneShotSound(char* soundName)
+{
+	int _soundIndex = -1;	//Index of correct sound in the vectors
+	bool found = false;
+
+	//Go through the index vector to find index of correct sound
+	for (int i = 0; i < m_soundIndex.size() && found == false; i++)
+	{
+		if (m_soundIndex[i] == soundName)
+		{
+			found = true;
+			_soundIndex = i;
+		}
+	}
+
+	//If the sound was found, play it!
+	if (_soundIndex != -1) 
+	{
+		m_result = m_system->playSound(FMOD_CHANNEL_FREE, m_sounds[_soundIndex], false, 0);
+		FMODErrorCheck(m_result);
+	}
+	
+}
+
 void SoundManager::FMODErrorCheck(FMOD_RESULT result)
 {
 	if (result != FMOD_OK)
@@ -26,10 +76,6 @@ void SoundManager::FMODErrorCheck(FMOD_RESULT result)
 		MessageBoxAndShutDown(&_ss);
 	}
 }
-
-
-
-
 
 void SoundManager::InitFMOD()
 {
@@ -92,5 +138,16 @@ void SoundManager::InitFMOD()
 
 	//Actually initialize the FMOD system
 	m_result = m_system->init(100, FMOD_INIT_NORMAL, 0);	//100 is number of virtual channel, won't probably use more than 5
+
+	//If the selected speaker mode isn't supported by this sound card, switch it back to stereo
+	if (m_result == FMOD_ERR_OUTPUT_CREATEBUFFER)
+	{
+		m_result = m_system->setSpeakerMode(FMOD_SPEAKERMODE_STEREO);
+		FMODErrorCheck(m_result);
+
+		m_result = m_system->init(100, FMOD_INIT_NORMAL, 0);
+	}
 	FMODErrorCheck(m_result);
 }
+
+
