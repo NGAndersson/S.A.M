937ec6626d6d7632f70ffccf35505c23106f2fe0
yorkhawke <yorkhawke@gmail.com>
2016-01-27 10:41:34 +0100

    screenManager some mine kod to start with.

---
+++ S.A.M/S.A.M/Header Files/ScreenManager.h
@@ -0,0 +1,46 @@
+#ifndef SCREENMANAGER
+#define SCREENMANAGER
+
+#define START			0;
+#define USERINTERFACE	1;
+#define HIGHSCORE		2;
+#define OPTION			3;
+#define PAUSE			4;
+#define ENDSCREEN		5;
+
+
+//Includes
+//1.Diff Screens
+//2.input
+//3.modelhandler
+
+class ScreenManager
+{
+//----------------------------Functions----------------------------------------
+private:
+	//Destructor and Constructor
+	ScreenManager();
+	~ScreenManager();
+	//Uppdate screens
+	void Uppdate(int CurrentScreen);
+	//Initializing the screen manager
+	void InitializeScreen();
+	//Rendercall
+	void Render();
+
+//----------------------------Attributes----------------------------------------
+private:
+	//Current status.
+	int mCurrent;
+	//Different screens
+
+
+	//input
+
+
+	//Modelhandler
+
+
+};
+
+#endif
