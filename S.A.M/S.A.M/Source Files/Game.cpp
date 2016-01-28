61f36d259d801e45ce0b6f0b0c0d929121f5894f
Downsight <leif.andreas.persson@gmail.com>
2016-01-28 15:32:49 +0100

    fixed directx initialization

--- S.A.M/S.A.M/Source Files/Game.cpp
+++ S.A.M/S.A.M/Source Files/Game.cpp
@@ -4,6 +4,34 @@ Game::Game()
 {
 }
 
+Game::~Game()
+{
+	if (m_deviceContext)
+	{
+		m_deviceContext->Release();
+	}
+	if (m_device)
+	{
+		m_device->Release();
+	}
+	if (m_swapChain)
+	{
+		m_swapChain->Release();
+	}
+	if (m_backbufferRTV)
+	{
+		m_backbufferRTV->Release();
+	}
+	if (m_depthStencilView)
+	{
+		m_depthStencilView->Release();
+	}
+	if (m_depthStencil)
+	{
+		m_depthStencil->Release();
+	}
+}
+
 void Game::InitGame(Input* input, Display* disp)
 {
 	m_input = input;
@@ -13,11 +41,14 @@ void Game::InitGame(Input* input, Display* disp)
 	//Create and initialize SoundManager
 	m_soundManager = new SoundManager;  //Initializes in constructor
 
+	//Create and initialize device/devicecontext/swapchain/depthstenciel
+	CreateDirect3DContext(disp->GethWnd());
+
 	//FUN STUFF! REMOVE!
 	m_soundManager->LoadSound("Resources/wave.mp3", "wave", "music", LOAD_STREAM);
 	m_soundManager->LoadSound("Resources/Song.mp3", "gangnam", "music", LOAD_STREAM);
 	m_soundManager->PlayOneShotSound("music", 0.5f);
-	m_soundManager->PauseSound("music");
+
 
 
 	//Create and initialize ScreenManager
@@ -50,9 +81,9 @@ WPARAM Game::MainLoop()
 
 		//Get Time
 		//time = Time.GetTime() ?
-
-		CheckInput();
-
+		double haj = 0;
+		m_input->CheckKeyBoardInput(haj);
+		m_input->CheckMouseInput(haj);
 		//Call update functions
 		Update(_time.TimeCheck());
 
@@ -82,4 +113,83 @@ void Game::CheckInput()
 	if (_returnInput & INPUT_ESC)
 		exit(0);
 	m_input->CheckMouseInput(haj);
+}
+
+void Game::SetViewport()
+{
+	D3D11_VIEWPORT _viewport;
+	_viewport.Width = (float)640;
+	_viewport.Height = (float)480;
+	_viewport.MinDepth = 0.0f;
+	_viewport.MaxDepth = 1.0f;
+	_viewport.TopLeftX = 0;
+	_viewport.TopLeftY = 0;
+	m_deviceContext->RSSetViewports(1, &_viewport);
+}
+
+HRESULT Game::CreateDirect3DContext(HWND wndHandle)
+{
+	// create a struct to hold information about the swap chain
+	DXGI_SWAP_CHAIN_DESC _swapChainDesc;
+
+	// clear out the struct for use
+	ZeroMemory(&_swapChainDesc, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+	// fill the swap chain description struct
+	_swapChainDesc.BufferCount = 1;                                    // one back buffer
+	_swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;     // use 32-bit color
+	_swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;      // how swap chain is to be used
+	_swapChainDesc.OutputWindow = wndHandle;                           // the window to be used
+	_swapChainDesc.SampleDesc.Count = 1;                               // how many multisamples
+	_swapChainDesc.Windowed = TRUE;                                    // windowed/full-screen mode
+
+															// create a device, device context and swap chain using the information in the scd struct
+	HRESULT hr = D3D11CreateDeviceAndSwapChain(NULL,
+		D3D_DRIVER_TYPE_HARDWARE,
+		NULL,
+		D3D11_CREATE_DEVICE_DEBUG,
+		NULL,
+		NULL,
+		D3D11_SDK_VERSION,
+		&_swapChainDesc,
+		&m_swapChain,
+		&m_device,
+		NULL,
+		&m_deviceContext);
+
+	DepthStencilInitialicer(); //skapar depthstencil/desc 
+
+	if (SUCCEEDED(hr))
+	{
+		// get the address of the back buffer
+		ID3D11Texture2D* pBackBuffer = nullptr;
+		m_swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+
+		// use the back buffer address to create the render target
+		HRESULT hr = m_device->CreateRenderTargetView(pBackBuffer, NULL, &m_backbufferRTV);
+		pBackBuffer->Release();
+
+		// set the render target as the back buffer
+		m_deviceContext->OMSetRenderTargets(1, &m_backbufferRTV, m_depthStencilView);
+	}
+	return hr;
+}
+
+void Game::DepthStencilInitialicer()
+{
+	//create the depth stencil
+	D3D11_TEXTURE2D_DESC _descDepth;
+	_descDepth.Width = 640;
+	_descDepth.Height = 480;
+	_descDepth.MipLevels = 1;
+	_descDepth.ArraySize = 1;
+	_descDepth.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	_descDepth.SampleDesc.Count = 1;
+	_descDepth.SampleDesc.Quality = 0;
+	_descDepth.Usage = D3D11_USAGE_DEFAULT;
+	_descDepth.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	_descDepth.CPUAccessFlags = 0;
+	_descDepth.MiscFlags = 0;
+	m_device->CreateTexture2D(&_descDepth, NULL, &m_depthStencil);
+	m_device->CreateDepthStencilView(m_depthStencil, NULL, &m_depthStencilView);
 }
