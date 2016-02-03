bfbea47b80ba82ae5d3eccd6e92f7ff56caec89f
SwedishSock <radditz770@gmail.com>
2016-02-03 11:43:29 +0100

    Added enum Bullettype to bullet-class

--- S.A.M/S.A.M/Header Files/Bullet.h
+++ S.A.M/S.A.M/Header Files/Bullet.h
@@ -2,6 +2,16 @@
 #define BULLET_H
 #include "Entity.h"
 
+enum BulletType
+{
+	PLAYER_1,	//basic shot (finger snapping)
+	PLAYER_2,	//slowing shot (electro drumkick)
+	PLAYER_3,	//shot 3
+	PLAYER_4,	//shot 4
+	PLAYER_5,	//shot 5
+	ENEMY_1		//enemy's shot
+};
+
 class Bullet : Entity
 {
 	//Functions
